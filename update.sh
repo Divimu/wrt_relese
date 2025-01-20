fdba752d5e57067573f20b6dc96de956b08b97e1
Divimu <deng_9527@163.com>
2025-01-21 01:39:02 +0800

    Update update.sh
    Signed-off-by: Divimu <deng_9527@163.com>

--- update.sh
+++ update.sh
@@ -132,8 +132,6 @@ fix_default_set() {
         find "$BUILD_DIR/feeds/divim/luci-theme-argon" -type f -name "cascade*" -exec sed -i 's/--bar-bg/--primary/g' {} \;
     fi
 
-    install -Dm755 "$BASE_PATH/patches/99_set_argon_primary" "$BUILD_DIR/package/base-files/files/etc/uci-defaults/99_set_argon_primary"
-
     if [ -f "$BUILD_DIR/package/emortal/autocore/files/tempinfo" ]; then
         if [ -f "$BASE_PATH/patches/tempinfo" ]; then
             \cp -f "$BASE_PATH/patches/tempinfo" "$BUILD_DIR/package/emortal/autocore/files/tempinfo"
